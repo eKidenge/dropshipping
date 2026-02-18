08bcef40fa90501c08052840b931453da5b76be3
Elisha Kidenge <elishakidenge@gmail.com>
2026-02-18 16:52:00 +0300

    Update start command

--- start.sh
+++ start.sh
@@ -3,12 +3,16 @@
 
 python manage.py migrate
 
-# Create admin if doesn't exist
+# Create admin using environment variables
 python manage.py shell -c "
 from django.contrib.auth import get_user_model;
+import os;
 User = get_user_model();
-if not User.objects.filter(username='admin').exists():
-    User.objects.create_superuser('admin', 'admin@dropshipping.com', 'admin123')
+username = os.environ.get('DJANGO_ADMIN_USERNAME', 'admin');
+email = os.environ.get('DJANGO_ADMIN_EMAIL', 'admin@dropshipping.com');
+password = os.environ.get('DJANGO_ADMIN_PASSWORD', 'admin123');
+if not User.objects.filter(username=username).exists():
+    User.objects.create_superuser(username, email, password)
 "
 
-gunicorn dropshipping.wsgi:application
+gunicorn dropshipping.wsgi:application --bind 0.0.0.0:\$PORT
